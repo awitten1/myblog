5303345635207ef9b1fe7e6c81e097467b31297f
Andrew Witten <awitten007@gmail.com>
2026-01-02 16:11:01 -0500

    update tests

--- code/binary-search/main.cpp
+++ code/binary-search/main.cpp
@@ -37,13 +37,16 @@ static void BM_LinearSearch(benchmark::State& state) {
     state.SetComplexityN(N);
 }
 
+const long end_dense_first = 1 << 8;
 const long end_dense = 1 << 11;
 const long end_range = 1 << 18;
 
-BENCHMARK(BM_LinearSearch)->DenseRange(8,end_dense,1<<7)
+BENCHMARK(BM_LinearSearch)->DenseRange(8,end_dense_first,1<<5)
+    ->DenseRange(end_dense_first,end_dense,1<<7)
     ->RangeMultiplier(2)->Range(end_dense, end_range)
     ->Complexity();
-BENCHMARK(BM_BinarySearch)->DenseRange(8,end_dense,1<<7)
+BENCHMARK(BM_LinearSearch)->DenseRange(8,end_dense_first,1<<6)
+    ->DenseRange(end_dense_first,end_dense,1<<7)
     ->RangeMultiplier(2)->Range(end_dense, end_range)
     ->Complexity();
 
