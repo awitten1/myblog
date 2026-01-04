6af278fe1a29723a83512896aa4125373c4dc848
Andrew Witten <awitten1@terpmail.umd.edu>
2026-01-04 16:49:14 -0500

    add instructions

--- app/components/plots.tsx
+++ app/components/plots.tsx
@@ -10,9 +10,11 @@ const ROOT_DIR = process.env.ROOT_DIR || process.cwd()
 export async function LinePlot({
   height,
   width,
+  yaxis
 }: {
   height: number;
   width: number;
+  yaxis: string;
 }) {
   const connection = await DuckDBConnection.create();
 
@@ -25,7 +27,7 @@ export async function LinePlot({
           substr(name,position('/' in name)+1,
             length(name)-position('/' in name))::int as input_size
         from '${ROOT_DIR}/${file}'
-        where cycles is not null and input_size < (1 << 12)
+        where cycles is not null and input_size < (1 << 10)
         `
   }
   const query = template_query('/code/binary-search/results/amdzen5/cycles.csv',)
@@ -33,35 +35,30 @@ export async function LinePlot({
   await connection.run(query)
 
   let reader = await connection.runAndReadAll(`select max(input_size) max_input_size,
-    max(cycles) max_cycle_count from results`)
-  let { max_input_size, max_cycle_count } = reader.getRowObjects()[0];
+    max(${yaxis}) max_yaxis from results`)
+  let { max_input_size, max_yaxis } = reader.getRowObjects()[0];
 
   async function getPoints(alg, connection, name) {
     const reader = await connection.runAndReadAll(
-      `select cycles,input_size from results
+      `select ${yaxis},input_size from results
       where algorithm = '${alg}'
       order by input_size asc`);
     const rows = reader.getRowObjects();
-    const points = rows.map((obj) => {return {x: obj.input_size,y: obj.cycles}})
+    const points = rows.map((obj) => {return {x: obj.input_size,y: obj[yaxis]}})
 
     return {points, className: alg, name: name}
   }
 
-  const labelMap: Record<string, string> = {
-    'BinarySearch': 'Binary Search',
-    'LinearSearch': 'Linear Search'
-  };
-
-  let lines: Line[] = []
+  let lines: any[] = []
   lines.push(await getPoints('BinarySearch',connection,'Binary Search'))
   lines.push(await getPoints('LinearSearch',connection,'Linear Search'))
 
   return (
     <div className={'plotWrapper'}>
     <LinePlotClient height={height} width={width} maxx={max_input_size?.valueOf()}
-      maxy={max_cycle_count?.valueOf()}
+      maxy={max_yaxis?.valueOf()}
       lines={lines}
-      metadata={{xName: 'number of elements', yName: 'Cycles'}}
+      metadata={{xName: 'number of elements', yName: yaxis}}
     />
     </div>
   )
