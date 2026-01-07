e3f7dc17732b4c9154d367b0dc050e53fd639832
Andrew Witten <awitten1@terpmail.umd.edu>
2026-01-07 14:29:26 -0500

    install libpfmm from source

--- code/utils/install-gbench.sh
+++ code/utils/install-gbench.sh
@@ -12,6 +12,20 @@ mkdir -p $install_dir
 mkdir -p $build_dir
 mkdir -p $source_dir
 
+install_libpfm() {
+    local distro=$(cat /etc/os-release | grep ^NAME | cut -d'=' -f2 | tr -d '"')
+    if [ ${distro} = 'Ubuntu' ]; then
+        sudo apt install -y libpfm4-dev
+    else
+        pushd $source_dir
+        git clone --branch v4.13.0 https://github.com/wcohen/libpfm4.git
+        pushd libpfm4
+        make PREFIX=$install_dir install
+        popd
+        popd
+    fi
+}
+
 install_google_benchmark() {
     local gbench_version=v1.9.4
 
@@ -24,14 +38,11 @@ install_google_benchmark() {
     local enable_libpfm=OFF
     if [ $(uname -s) = 'Linux' ]; then
         enable_libpfm=ON
-        local distro=$(cat /etc/os-release | grep ^NAME | cut -d'=' -f2 | tr -d '"')
-        if [ ${distro} = 'Ubuntu' ]; then
-            sudo apt install -y libpfm4-dev
-        fi
+        install_libpfm
     fi
     cmake -B $build_dir/benchmark -S benchmark -DCMAKE_INSTALL_PREFIX=$install_dir \
         -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON -G Ninja \
-        -DBENCHMARK_ENABLE_LIBPFM=${enable_libpfm}
+        -DBENCHMARK_ENABLE_LIBPFM=${enable_libpfm} -DCMAKE_PREFIX_PATH=$install_dir
 
     cmake --build $build_dir/benchmark -j8
     cmake --install $build_dir/benchmark
